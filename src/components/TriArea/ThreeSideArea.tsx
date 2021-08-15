af30654c6518dfed55a98fc29d713e6a2faf5efe
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-15 17:36:57 +0530

    feat: implemented area page and lbArea component

--- src/components/TriArea/ThreeSideArea.tsx
+++ src/components/TriArea/ThreeSideArea.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default () => {
   const [side1, setSide1] = useState<number>(NaN);
   const [side2, setSide2] = useState<number>(NaN);
-  // const [side3, setSide3] = useState<number>(NaN);
+  const [side3, setSide3] = useState<number>(NaN);
   const [valall, setValall] = useState(true);
   const [isCalcDone, setIsCalcDone] = useState(false);
   const invalidateSidele = (e: number): boolean =>
@@ -11,18 +11,19 @@ export default () => {
 
   return (
     <>
+      3side
       <div className="bcard">
         <header className="head">
-          <h1>Enter the height and base </h1>
+          <h1>Enter the lengths of sides of right angle triangle</h1>
         </header>
 
         <section className="input">
           <label>
-            <span className="label">height</span>
+            <span className="label">Sidele 1</span>
             <input
               type="number"
               value={side1}
-              placeholder={"Enter height"}
+              placeholder={"Enter Sidele 1"}
               onChange={(e) => {
                 setSide1(parseFloat(e.target.value));
                 setIsCalcDone(false);
@@ -30,11 +31,11 @@ export default () => {
             />
           </label>
           <label>
-            <span className="label">Base</span>
+            <span className="label">Sidele 2</span>
             <input
               type="number"
               value={side2}
-              placeholder={"Enter Base"}
+              placeholder={"Enter Sidele 2"}
               onChange={(e) => {
                 setSide2(parseFloat(e.target.value));
                 setIsCalcDone(false);
@@ -59,7 +60,9 @@ export default () => {
         <span>
           {valall
             ? isCalcDone &&
-              "Area is equal to " + (0.5 * side1 * side2).toFixed(2) + "."
+              "Hypotenuse is equal to " +
+                Math.sqrt(side1 * side1 + side2 * side2).toFixed(2) +
+                "."
             : "Please enter values greater than 0 (only numbers are allowed in above fields)"}
         </span>
       </div>
--- src/components/TriArea/TriArea.tsx
+++ src/components/TriArea/TriArea.tsx
@@ -1,71 +1,47 @@
 import { useState } from "react";
 import "./TriArea.css";
-const TriArea = () => {
-  const [side1, setSide1] = useState<number>(NaN);
-  const [side2, setSide2] = useState<number>(NaN);
-  const [side3, setSide3] = useState<number>(NaN);
-  const [valall, setValall] = useState(true);
-  const [isCalcDone, setIsCalcDone] = useState(false);
-  const invalidateSidele = (e: number): boolean =>
-    isNaN(e) || e < 0.000000000001;
+import LbArea from "./LbArea";
+import SideAngleArea from "./SideAngleArea";
+import ThreeSideArea from "./ThreeSideArea";
+const methodList = [
+  "Base and height",
+  "Three sides",
+  "Two sides and included angle",
+];
+const data: { [key: string]: JSX.Element } = {
+  "Base and height": <LbArea />,
+  "Three sides": <SideAngleArea />,
+  "Two sides and included angle": <ThreeSideArea />,
+};
 
+export default () => {
+  const [method, setMethod] = useState(methodList[0]);
   return (
-    <>
-      <div className="bcard">
-        <header className="head">
-          <h1>Enter the lengths of sides of right angle triangle</h1>
-        </header>
-
-        <section className="input">
-          <label>
-            <span className="label">Sidele 1</span>
-            <input
-              type="number"
-              value={side1}
-              placeholder={"Enter Sidele 1"}
-              onChange={(e) => {
-                setSide1(parseFloat(e.target.value));
-                setIsCalcDone(false);
-              }}
-            />
-          </label>
-          <label>
-            <span className="label">Sidele 2</span>
-            <input
-              type="number"
-              value={side2}
-              placeholder={"Enter Sidele 2"}
-              onChange={(e) => {
-                setSide2(parseFloat(e.target.value));
-                setIsCalcDone(false);
-              }}
-            />
-          </label>
-
-          <button
-            onClick={() => {
-              if (invalidateSidele(side1) || invalidateSidele(side2)) {
-                setValall(false);
-              } else {
-                setIsCalcDone(true);
-                setValall(true);
-                console.log(isCalcDone);
-              }
-            }}
-          >
-            Check
-          </button>
-        </section>
-        <span>
-          {valall
-            ? isCalcDone &&
-              "Hypotenuse is equal to " +
-                Math.sqrt(side1 * side1 + side2 * side2).toFixed(2) +
-                "."
-            : "Please enter values greater than 0 (only numbers are allowed in above fields)"}
-        </span>
+    <div className="bcard mid-cover">
+      <h2 className="fix">
+        Choose the inputs by which you want to calculate area
+      </h2>
+      <div className="method-list">
+        {methodList.map((i) => {
+          return (
+            <>
+              <span
+                className={"method-tile " + (method == i ? "highlight" : "")}
+                onClick={() => {
+                  setMethod(i);
+                }}
+                key={i}
+              >
+                {i}
+              </span>
+            </>
+          );
+        })}
       </div>
-    </>
+      {/* this is the best pices of code I wrote today cause it made me happy I dont know weather it is the way to do it or not 
+      but it was definitly worth it !
+      */}
+      {(() => data[method])()}
+    </div>
   );
 };
-export default TriArea;
