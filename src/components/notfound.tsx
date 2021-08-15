18aa686a3ad03362871c11bfc9f7e64ca15cded5
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-15 16:25:12 +0530

    style: centered  no page found

--- src/components/notfound.tsx
+++ src/components/notfound.tsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 const NotFound = () => {
   return (
-    <div className="mid-cover">
+    <div className="mid-cover text-center">
       <h2 className="msg-title">Page not Found!</h2>
     </div>
   );
