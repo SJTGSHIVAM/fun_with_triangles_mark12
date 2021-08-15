17dd21ae96570a1c02b83631aac08d466e9d0055
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-15 17:56:25 +0530

    feat: implemented 3 sides

--- src/components/TriArea/SideAngleArea.tsx
+++ src/components/TriArea/SideAngleArea.tsx
@@ -5,10 +5,12 @@ export default () => {
   const [side2, setSide2] = useState<number>(NaN);
   const [side3, setSide3] = useState<number>(NaN);
   const [valall, setValall] = useState(true);
+  const [valTri, setValTri] = useState(true);
   const [isCalcDone, setIsCalcDone] = useState(false);
   const invalidateSidele = (e: number): boolean =>
     isNaN(e) || e < 0.000000000001;
-
+  const validateTriangle = (side1: number, side2: number, side3: number) =>
+    side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2;
   return (
     <>
       2side
@@ -42,10 +44,27 @@ export default () => {
               }}
             />
           </label>
+          <label>
+            <span className="label">Sidele 3</span>
+            <input
+              type="number"
+              value={side3}
+              placeholder={"Enter Sidele 3"}
+              onChange={(e) => {
+                setSide3(parseFloat(e.target.value));
+                setIsCalcDone(false);
+              }}
+            />
+          </label>
 
           <button
             onClick={() => {
-              if (invalidateSidele(side1) || invalidateSidele(side2)) {
+              if (
+                invalidateSidele(side1) ||
+                invalidateSidele(side2) ||
+                invalidateSidele(side3) ||
+                !validateTriangle(side1, side2, side3)
+              ) {
                 setValall(false);
               } else {
                 setIsCalcDone(true);
@@ -58,12 +77,17 @@ export default () => {
           </button>
         </section>
         <span>
-          {valall
+          {valall && valTri
             ? isCalcDone &&
-              "Hypotenuse is equal to " +
-                Math.sqrt(side1 * side1 + side2 * side2).toFixed(2) +
+              "Area is equal to " +
+                Math.sqrt(
+                  ((side1 + side2 + side3) / 2) *
+                    ((side1 + side2 + side3) / 2 - side1) *
+                    ((side1 + side2 + side3) / 2 - side2) *
+                    ((side1 + side2 + side3) / 2 - side3)
+                ).toFixed(2) +
                 "."
-            : "Please enter values greater than 0 (only numbers are allowed in above fields)"}
+            : "Please enter values greater than 0 (only numbers are allowed in above fields and sum of two sides should be greater than third)"}
         </span>
       </div>
     </>
