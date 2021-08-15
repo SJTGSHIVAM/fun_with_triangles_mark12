fd13f29647fa8971a2753ef518a5b51002a712df
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-15 14:19:03 +0530

    feat: navbar, About and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./components/About";
+
+import NotFound from "./components/notfound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {" "}
+      <Navbar />
+      <Switch>
+        <Route path="/about" exact component={About} />
+        <Route path="/" component={NotFound} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
